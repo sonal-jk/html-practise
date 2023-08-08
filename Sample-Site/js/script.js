621589ccd9c0511b0375fe36be92f799525bc258
sonal-jk <sonal1024jk@gmail.com>
2023-08-08 21:48:24 +0530

    commit and push

---
+++ Sample-Site/js/script.js
@@ -0,0 +1,10 @@
+var a=document.querySelector(".g");
+var b=document.querySelector(".cont");
+function show(){
+    b.style.display='block';
+    a.style.backgroundColor="red";
+}
+function hide(){
+    
+    b.style.display='none';
+}
